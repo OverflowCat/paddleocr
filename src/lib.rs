8f256fc5f097d312a72c94f3168aa125d0dc9c50
猫猫 <overflowcat@gmail.com>
2022-10-09 00:48:14 +0800

    feat: POC of OCRing a single image

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::io::{BufRead, BufReader};
+use core::panic;
+use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::{error::Error, ffi::OsString, fmt, path::PathBuf, time::Duration};
 
 use std::process;
@@ -13,6 +14,7 @@ impl fmt::Display for OsNotSupportedError {
 impl Error for OsNotSupportedError {}
 pub struct Ppocr {
     exe_path: PathBuf,
+    process: process::Child,
 }
 
 impl Ppocr {
@@ -26,42 +28,50 @@ impl Ppocr {
         println!("{:?}", std::env::current_dir()?);
         let wd = OsString::from(exe_path.parent().unwrap());
         std::env::set_current_dir(&wd).unwrap();
-        let mut sp = process::Command::new("python3")
+        let mut sp = process::Command::new(&exe_path)
             .args(&[
-                "-c",
-                r#"from time import sleep
-sleep(0.01)
-print("a", end="_")
-sleep(0.2)
-print("b")
-print('{some: "json!"}')
-sleep(1)
-print("process ended!!!")"#,
+                "--det_model_dir=ch_PP-OCRv3_det_infer",
+                "--cls_model_dir=ch_ppocr_mobile_v2.0_cls_infer",
+                "--rec_model_dir=ch_PP-OCRv3_rec_infer",
+                " --rec_char_dict_path=ppocr_keys_v1.txt",
             ])
             .stdout(process::Stdio::piped())
             .stderr(process::Stdio::piped())
+            .stdin(process::Stdio::piped())
             .spawn()?;
-        /* exe_path.to_str().unwrap(),
-        "--det_model_dir=ch_PP-OCRv3_det_infer",
-        "--cls_model_dir=ch_ppocr_mobile_v2.0_cls_infer",
-        "--rec_model_dir=ch_PP-OCRv3_rec_infer",
-        " --rec_char_dict_path=ppocr_keys_v1.txt", */
+
         let mut sstdout = BufReader::new(sp.stdout.as_mut().unwrap());
         // let mut sstderr = BufReader::new(sp.stderr.as_mut().unwrap());
+        let sstdin = sp.stdin.as_mut().unwrap();
+
+        // initializing
         let mut buff = String::new();
-        for _i in 1..50 {
+        for _i in 1..8 {
             match sstdout.read_line(&mut buff) {
-                Ok(siz) => {
-                    println!("Read size {}: {}", siz, buff);
+                Ok(_) => {
+                    println!("《{}》", buff);
+                    if buff.starts_with("OCR init completed.") {
+                        println!("OCR 初始化成功！");
+                        let image_path = "C:\\Users\\Neko\\Pictures\\test.png";
+                        sstdin.write_fmt(format_args!("{}\n", image_path)).unwrap();
+                    }
                     buff.clear();
                 }
                 Err(e) => {
                     println!("读取 stdout 发生错误：{:?}", e);
-                    break;
+                    panic!()
                 }
             }
         }
-        Ok(Ppocr { exe_path })
+        Ok(Ppocr {
+            exe_path,
+            process: sp,
+        })
+    }
+    pub fn read_line() {}
+    pub fn write() {}
+    pub fn ocr(image_path: PathBuf) -> String {
+        String::new()
     }
 }
 
@@ -71,7 +81,6 @@ mod tests {
     #[test]
     fn it_works() {
         let api = Ppocr::new(PathBuf::from(
-            // "C:/Users/Neko/Documents/GitHub/cwdinspect/target/release/cwdinspect.exe"
             "C:/Users/Neko/Documents/GitHub/paddleocr/PaddleOCR-json/PaddleOCR_json.exe",
         ));
         api.unwrap();
